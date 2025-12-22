fc2f2c6036d7233e0c0f62f244da4d185f0c51a8
konrad <konradkaczma1@gmail.com>
2025-12-22 21:09:40 +0100

    koniec tury, gracze oraz punkty, liczba graczy oraz indywidualne talie kart

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
+import random
+import math
 
 # Example file showing a basic pygame "game loop"
 import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((720, 720))
+screen = pygame.display.set_mode((800, 720))
 clock = pygame.time.Clock()
 running = True
 
@@ -14,18 +16,24 @@ def board_draw():
     x, y = 0, 0
     color = (0,0,0)
     for i in range(7):
-        x=i*64
+        x=i*80
         for j in range(7):
-            y=j*64
-            if i%2==0:
-                if j%2==0: color = (200,200,200)
-                else: color = (100,100,100)
-            else:
-                if j%2==0: color = (100,100,100)
-                else: color = (200,200,200)
-            pygame.draw.rect(screen, color, (x , y, 64, 64))
+            y=j*80
+            if (j%2==0 and i%2==0) or (i%2==1 and j%2==1): color = (200,200,200)
+            else: color = (100,100,100)
+            pygame.draw.rect(screen, color, (x , y, 78, 78))
 
+def stat_draw():
+    x = 580
+    y = 20
+    for i in range(NumberOfPlayers):
+        pygame.draw.rect(screen, (60,110,200), (x, y, 200, 60))
+        text = font.render(f"Gracz {str(i+1)}: {punkty[i]}", True, (255, 255, 255))
+        screen.blit(text, (x+10, y+20))
+        y+=80
 
+def end_turn_draw():
+    pygame.draw.rect(screen, (200,50,0), (640, 480, 80, 80))
 def end_turn():
     pass
 
@@ -41,6 +49,9 @@ board = [["","","","","","",""],
          ["","","","","","",""],
          ["","","","","","",""]]
 
+#dostępny karty
+classes = ["Pikeman", "Miner", "Mage", "Archer", "Cannon", "Cavalery", "Healer"]
+
 #wgrywanie obrazków
 card_img = pygame.image.load("images/temp.jpg").convert_alpha() # dla img z transparencją
 card_img = pygame.transform.scale(card_img, (128,128))
@@ -52,29 +63,34 @@ cards = pygame.Surface((400,400), pygame.SRCALPHA)
 cards.blit(card_img, (0, 20))
 cards.blit(card_img, (10, 30))
 
-#można rónież pisać jeśli byloby to potrzebne
-font = pygame.font.Font(None, size=30)
-"""
-text = font.render("Hello world ", True, (0,0,0))
-screen.blit(text,(50,300))   wyświetlajie tekstu, to oczywiscie w loopie
-    """
-
 #ważny temat, rect-y
 kwadrat1 = pygame.Rect(0,0,50,50) #pozycjja, wymiary
 kwadrat2 = pygame.Rect(300,300,300,300) #pozycjja, wymiary
 kolizja = kwadrat1.colliderect(kwadrat2) #zwraca true or false
 pygame.draw.rect(screen,(100,100,100), kwadrat2) #surface, color, what rectangle to draw
 
-while running:
-    screen.fill((255,255,255))  # miejsce na board_draw()
-    #słynne event_loop
-    board_draw()
+#miejsce za potrzebne zmienne
+NumberOfPlayers = 4
+punkty = [0,0,0,0]
+font = pygame.font.Font(None, size=30)
+decks = []
 
+for i in range(NumberOfPlayers):
+    deck = []
+    for j in range(15):
+        deck.append(classes[random.randint(1, 100)%7])
+    decks.append(deck)
+
+
+while running:
+    screen.fill((255, 238, 177))  # miejsce na board_draw()
+
+    board_draw()
+    stat_draw()
+    end_turn_draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #pygame.draw.rect(screen,(255,100,100), kwadrat2)
-    #screen.blit(cards, (0,0))
 
     # flip() przeniesienie modyfikacji powierzchni screen na display
     pygame.display.flip()
